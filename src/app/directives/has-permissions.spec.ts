5cb76cb2ed115bd473c5a71c7fac7e20d2dfb648
Bruno Honda <bruno.honda@live.com>
2026-02-09 15:18:16 -0300

    feat: implements HasPermissions directive to using signals and effect

--- src/app/directives/has-permissions.spec.ts
+++ src/app/directives/has-permissions.spec.ts
@@ -1,8 +1,57 @@
+import { Component, input } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Mock } from '@vitest/spy';
+import { AuthService } from '../services/auth-service';
+import { BooleanExpression } from '../types/boolean-expression';
 import { HasPermissions } from './has-permissions';
 
-describe('HasPermissions', () => {
-  it('should create an instance', () => {
-    const directive = new HasPermissions();
-    expect(directive).toBeTruthy();
+
+@Component({
+  standalone: true,
+  imports: [HasPermissions],
+  template: `
+    <div *appHasPermissions="permissions()">
+      <p>Hello World</p>
+    </div>
+  `
+})
+class MockComponent {
+  permissions = input<BooleanExpression>('PERMISSION_1');
+}
+
+describe.only(HasPermissions.name, () => {
+  let fixture: ComponentFixture<MockComponent>;
+  let component: MockComponent;
+  let hasPermissionsSpy: Mock<(permission: BooleanExpression) => boolean>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MockComponent],
+    }).compileComponents();
+
+    const authService = TestBed.inject(AuthService);
+    hasPermissionsSpy = vi.spyOn(authService, 'hasPermissions');
+
+    fixture = TestBed.createComponent(MockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create child', () => {
+    hasPermissionsSpy.mockReturnValue(true);
+    fixture.componentRef.setInput('permissions', 'PERMISSION_1');
+    fixture.detectChanges();
+
+    const content = fixture.nativeElement.querySelector('p');
+    expect(content).not.toBeNull();
+  });
+
+  it.only('should not create child', () => {
+    hasPermissionsSpy.mockReturnValue(false);
+    fixture.componentRef.setInput('permissions', 'PERMISSION_2');
+    fixture.detectChanges();
+
+    const content = fixture.nativeElement.querySelector('p');
+    expect(content).toBeNull();
   });
 });
--- src/app/directives/has-permissions.ts
+++ src/app/directives/has-permissions.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import { Directive, effect, inject, input, InputSignal, TemplateRef, ViewContainerRef } from '@angular/core';
+import { AuthService } from '../services/auth-service';
+import { BooleanExpression } from '../types/boolean-expression';
 
 @Directive({
   selector: '[appHasPermissions]',
+  standalone: true,
 })
 export class HasPermissions {
+  private templateRef = inject(TemplateRef);
+  private viewContainer = inject(ViewContainerRef);
 
-  constructor() { }
+  private _authService = inject(AuthService);
+    
+  permissions: InputSignal<BooleanExpression> = input.required({
+    alias: 'appHasPermissions',
+  });
 
+  constructor() {
+    effect(() => {
+      this.viewContainer.clear();
+      const permissions = this.permissions();
+
+      if (this._authService.hasPermissions(permissions)) {
+        this.viewContainer.createEmbeddedView(this.templateRef);
+      }
+    });
+  }
 }
